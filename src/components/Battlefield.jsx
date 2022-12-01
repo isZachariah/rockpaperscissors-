dae918ef0120a7f6e78adf384af76a2af856a488
isZachariah <103608503+isZachariah@users.noreply.github.com>
2022-12-01 14:17:21 -0800

    rebuilt with vite

--- src/components/Battlefield.jsx
+++ src/components/Battlefield.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Rock from '/images/Rock.png';
+import Paper from '/images/Paper.png';
+import Scissors from '/images/Scissors.png';
+
 
 export const Battlefield = ({player, computer, winner}) => {
 
@@ -6,10 +10,10 @@ export const Battlefield = ({player, computer, winner}) => {
         <div>
             <div className={'battlefield flex flex-row'}>
                 <div className="player-choice">
-                    <img className="player-img tr" src={`../images/${player}.png`} alt=''></img>
+                    <img className="player-img tr" src={`images/${player}.png`} alt=''></img>
                 </div>
                 <div className="computer-choice">
-                    <img className="computer-img" src={`../images/${computer}.png`} alt=''></img>
+                    <img className="computer-img" src={`images/${computer}.png`} alt=''></img>
                 </div>
             </div>
             <p className={'mt-12 text-3xl'}>{winner}</p>
